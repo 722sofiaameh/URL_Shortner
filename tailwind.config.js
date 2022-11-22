8107aa99336e08c919e1d86184eed64c54fef366
722sofiaameh <sofiagabriel722@gmail.com>
2022-11-22 07:57:50 -0800

    add updates to the landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        Cyan: "hsl(180, 66%, 49%)",
+        darkViolet: "hsl(257, 27%, 26%)",
+        Red: "hsl(0, 87%, 67%)",
+        Gray: "hsl(0, 0%, 75%)",
+        grayishViolet: "hsl(257, 7%, 63%)",
+        verydarkBlue: "hsl(255, 11%, 22%)",
+        verydarkViolet: "hsl(260, 8%, 14%)"
+      },
+      fontFamily:{
+        Poppins: ['Poppins',]
+      },
+      fontSize:{
+        bdFont : "18px"
+      }
+    },
   },
   plugins: [],
 }
